9df4beee321c5b8136c0ca328f8140057d6a5b3d
SmallNapkin99 <benceol1999@gmail.com>
2025-04-02 10:29:08 -0400

    wrapped browser router around app

--- jeopardy_front/src/App.js
+++ jeopardy_front/src/App.js
@@ -9,33 +9,31 @@ import ReadyUp from "./components/ReadyUp";
 import PlayerList from "./components/PlayerList";
 import Buzzer from "./components/Buzzer";
 import GameBoard from "./components/GameBoard";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/creategame" element={<CreateGame />} />
-        <Route path="/gamelist" element={<GameList />} />
-        <Route path="/game/:id" element={<GameAction />} />
-        <Route path="/game/:id/edit" element={<EditGame />} />
-        <Route
-          path="/game/:id/*"
-          element={
-            <WebSocketProvider>
-              <Routes>
-                <Route path="readyup" element={<ReadyUp />} />
-                <Route path="playerlist" element={<PlayerList />} />
-                <Route path="buzzer" element={<Buzzer />} />
-                <Route path="gameboard" element={<GameBoard />} />
-              </Routes>
-            </WebSocketProvider>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/creategame" element={<CreateGame />} />
+      <Route path="/gamelist" element={<GameList />} />
+      <Route path="/game/:id" element={<GameAction />} />
+      <Route path="/game/:id/edit" element={<EditGame />} />
+      <Route
+        path="/game/:id/*"
+        element={
+          <WebSocketProvider>
+            <Routes>
+              <Route path="readyup" element={<ReadyUp />} />
+              <Route path="playerlist" element={<PlayerList />} />
+              <Route path="buzzer" element={<Buzzer />} />
+              <Route path="gameboard" element={<GameBoard />} />
+            </Routes>
+          </WebSocketProvider>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
 
--- jeopardy_front/src/index.js
+++ jeopardy_front/src/index.js
@@ -4,9 +4,18 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import PageFrame from "./components/PageFrame";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<PageFrame content={<App />} />);
+root.render(
+  <PageFrame
+    content={
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    }
+  />
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
